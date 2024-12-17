152dd5ded4e84a5c1df63fb260584c9f781d4e26
Sakshi Maurya <126562319+SAKSHI2027@users.noreply.github.com>
2024-12-17 23:32:44 +0530

    Update pattwen2.java

--- pattwen2.java
+++ pattwen2.java
@@ -1,157 +1,173 @@
 
-////Q10  BUTTERFLY HAED PATTERNS
+//Q10  BUTTERFLY HAED PATTERNS
 
 
-// import java.util.*;
+import java.util.*;
 
-// public class patterns2 {
-//     public static void main(String args[]) {
-//         int n =5;
-//          ///upper half
-//      for (int i=1; i<= n; i++) {
-//         //first part
-//         for (int j=1; j<=i; j++) {
-//             System.out.print("*");
-//         }
-//           // spaces
-//             int spaces =2*(n-i);
-//             for (int j = 1; j<= spaces; j++){
-//                 System.out.print(" ");
-//             }
-//             // second part
-//             for (int j=1; j<=i;j++){
-//             System.out.print("*");
-//         }
-//          System.out.println();
-//        }
+public class patterns2 {
+    public static void main(String args[]) {
+        int n =5;
+         ///upper half
+     for (int i=1; i<= n; i++) {
+        //first part
+        for (int j=1; j<=i; j++) {
+            System.out.print("*");
+        }
+          // spaces
+            int spaces =2*(n-i);
+            for (int j = 1; j<= spaces; j++){
+                System.out.print(" ");
+            }
+            // second part
+            for (int j=1; j<=i;j++){
+            System.out.print("*");
+        }
+         System.out.println();
+       }
 
-//         ///lower half 
-//         for (int i=n; i>=1; i--) {
-//             //first part
-//             for (int j=1; j<=i; j++) {
-//                 System.out.print("*");
-//             }
-//               // spaces
-//                 int spaces = 2 * (n-i);
-//                 for (int j=1; j<= spaces; j++){
-//                     System.out.print(" ");
-//                 }
-//                 // second part
-//                 for (int j=1; j<=i;j++){
-//                 System.out.print("*");
-//             }
-//              System.out.println();
-//            }
-//     }
-// }
-    
-////Q 11 SOLID RHOMBUS HARD PATTERNS
+        ///lower half 
+        for (int i=n; i>=1; i--) {
+            //first part
+            for (int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+              // spaces
+                int spaces = 2 * (n-i);
+                for (int j=1; j<= spaces; j++){
+                    System.out.print(" ");
+                }
+                // second part
+                for (int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+             System.out.println();
+           }
+    }
+}
 
 
-// import java.util.*;
+
+//Q 11 SOLID RHOMBUS HARD PATTERNS
 
 
-// public class patterns2{
-//     public static void main (String args []){
-//         int n =5;
+import java.util.*;
+
+
+public class patterns2{
+    public static void main (String args []){
+        int n =5;
         
-//     for(int i=1;i<=n; i++){
-//             //spaces
-//            for(int j=1;j<=n-i; j++){
-//             System.out.print(" ");
-//            }
+    for(int i=1;i<=n; i++){
+            //spaces
+           for(int j=1;j<=n-i; j++){
+            System.out.print(" ");
+           }
 
-//            // stars 
-//            for (int j=1; j<=n; j++){
-//            System.out.print("*");
-//         }
-//         System.out.println();
-//       }
-//     }
-// }
+           // stars 
+           for (int j=1; j<=n; j++){
+           System.out.print("*");
+        }
+        System.out.println();
+      }
+    }
+}
   
+
+
 
   
 //Q 12 NUMBER PYRAMID
 
-// import java.util.*;
+
+import java.util.*;
 
 
-// public class patterns2{
-//     public static void main (String args []){
-//         int n =5;
-//         for (int i=1; i<=n; i++){
-//                // for space
-//                for (int j=1; j<=n-i; j++){
-//                 System.out.print(" ");
-//                }
-//                // nubers => print row number row number times
-//                for (int j=1; j<=i; j++ ){
-//                 System.out.print(i+" ");
+public class patterns2{
+    public static void main (String args []){
+        int n =5;
+        for (int i=1; i<=n; i++){
+               // for space
+               for (int j=1; j<=n-i; j++){
+                System.out.print(" ");
+               }
+               // nubers => print row number row number times
+               for (int j=1; j<=i; j++ ){
+                System.out.print(i+" ");
 
-//                }
-//                System.out.println();
-//             }    
-//         }
+               }
+               System.out.println();
+            }    
+        }
         
-//      }
-
-  /// Q 13 HOLLO SOLID RHOMBUS HARD PATTERNS
+     }
 
 
-// import java.util.*;
 
 
-// public class patterns2{
-//     public static void main (String args []){
-//         int n = 5;
-//         int m = 5;
+  // Q 13 HOLLO SOLID RHOMBUS HARD PATTERNS
+
+
+
+
+import java.util.*;
+
+
+public class patterns2{
+    public static void main (String args []){
+        int n = 5;
+        int m = 5;
         
-//     for(int i=1;i<=n; i++){
-//             //spaces
-//            for(int j=1;j<=n-i; j++){
-//             System.out.print(" ");
-//            }
+    for(int i=1;i<=n; i++){
+            //spaces
+           for(int j=1;j<=n-i; j++){
+            System.out.print(" ");
+           }
 
-//            // stars 
-//            for (int j=1; j<=n; j++){
-//             if (i == 1 || j == 1 || i == n || j == m) {
-//                  System.out.print("*");
-//             } else {
-//                  System.out.print(" ");
-//         }
+           // stars 
+           for (int j=1; j<=n; j++){
+            if (i == 1 || j == 1 || i == n || j == m) {
+                 System.out.print("*");
+            } else {
+                 System.out.print(" ");
+        }
         
-//         }
-//         System.out.println();
-//       }
-//     }
-// }
+        }
+        System.out.println();
+      }
+    }
+}
 
 
-// Q 14 palindromic pattens 
 
-// import java.util.*;
 
-// public class patterns2 {
-//     public static void main(String args[]){
-//      int n = 5;
-//     for(int i=1; i<=n; i++)  {
-//         // spaces
-//         for (int j=1; j<=n-i;j++){
-//             System.out.print(" ");
-//         }
-//         // 1st half number
-//         for (int j=i; j>=1; j--){
-//             System.out.print(j);
-//         }
-//         // 2nd hlaf
-//         for(int j=2; j<=i; j++){
-//             System.out.print(j);
-//         }
-//         System.out.println();
-//       }
-//     }
-// }
+//Q 14 palindromic pattens 
+
+
+
+import java.util.*;
+
+public class patterns2 {
+    public static void main(String args[]){
+     int n = 5;
+    for(int i=1; i<=n; i++)  {
+        // spaces
+        for (int j=1; j<=n-i;j++){
+            System.out.print(" ");
+        }
+        // 1st half number
+        for (int j=i; j>=1; j--){
+            System.out.print(j);
+        }
+        // 2nd hlaf
+        for(int j=2; j<=i; j++){
+            System.out.print(j);
+        }
+        System.out.println();
+      }
+    }
+}
+
+
 
 
 // Q 15 Dimond pattern
